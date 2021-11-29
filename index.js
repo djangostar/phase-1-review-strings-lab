9f36061bd0ab32b5543b22006ff9327fe86f6608
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-11-29 10:25:49 -0800

    commit tree e438884718dadf2f2b6fa4e35fbad4e585fa15c8

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Djangostarr';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// concate = const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+// Also works
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// .toUpperCase() = a DEFAULT string method that updates the value of the variable.
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!` 
+//const shortGreeting = 'Welcome, ' + currentUser.charAt(0) + '!';
+//works but it's less flexible and can be cleaned up, allowing flexibility with the .slice() method.
